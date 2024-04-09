273fc845fa091f37625f8e1eb794b9cfd216e6f3
Oshin-3 <oshin.gawande3@gmail.com>
2024-04-09 22:00:17 +0530

    watchlist is getting added to watchlist page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,27 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react'
 import Pagination from './Pagination'
+const _ = require("lodash");
 
 function Movies() {
 
     const [movies, setMovies] = useState([])
     //favorite movies list to add in the watchlist page
-    const [watchlist, setWatchlist] = useState([])
-
+    const [watchlist, setWatchlist] = useState(JSON.parse(localStorage.getItem('imdb')) || [])
+    
     //add to watchlist
-    const addToWatchlist = (id) => {
-        const newWatchlist = [...watchlist, id]
+    const addToWatchlist = (movie) => {
+        const newWatchlist = [...watchlist, movie]
         setWatchlist(newWatchlist)
+        localStorage.setItem('imdb', JSON.stringify(newWatchlist))
     }
 
     //remove from watchlist
-    const removeFromWatchlist = (id) => {
-        const filteredWatchlist = watchlist.filter((movie) => {
-            
-            return movie != id
+    const removeFromWatchlist = (movie) => {
+        const filteredWatchlist = watchlist.filter((m) => {
+
+            return m.id != movie.id
         })
         setWatchlist(filteredWatchlist)
+        localStorage.setItem('imdb', JSON.stringify(filteredWatchlist))
     }
+
+    //console.log(watchlist)
+
 
     //fetch the movies details from API using axios
     const fetchMovies = (pageNum) => {
@@ -34,9 +40,15 @@ function Movies() {
             })
     }
     useEffect(() => {
+        // let moviesFromLocalstorage = localStorage.getItem('imdb')
+        // moviesFromLocalstorage = JSON.parse(moviesFromLocalstorage) || []
+        // console.log("from local storage> ", moviesFromLocalstorage)
+
+        // setWatchlist(moviesFromLocalstorage)
+        console.log("Watchlist updated: ", watchlist);
         fetchMovies(1)
         
-    }, [])
+    },[])
 
 
     return (
@@ -46,7 +58,7 @@ function Movies() {
             </div>
             <div className='flex flex-wrap justify-center'>
                 {movies.map((movie) => {
-                    return <div key={movie.id}  className='w-[190px] h-[65vh] mt-5 mb-5 ml-4 mr-4 hover:scale-110 duration-300 rounded-lg'>
+                    return <div key={movie.id} className='w-[190px] h-[65vh] mt-5 mb-5 ml-4 mr-4 hover:scale-110 duration-300 rounded-lg'>
                         <div className='w-[190px] h-[40vh] bg-center bg-cover md:h[40vh] md:w[180px] rounded-t-lg'
                             style={{
                                 backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`
@@ -56,7 +68,7 @@ function Movies() {
                         <div className='w-[190px] h-[25vh] bg-zinc-900 rounded-b-lg pt-2'>
                             <div className='text-slate-50 w-full pl-2'>
                                 <span className='pr-2'><FontAwesomeIcon icon={faStar} color='gold' /></span>
-                                {movie.vote_average.toFixed(2) == 0 ? "Coming Soon" : movie.vote_average.toFixed(2)}
+                                {movie.vote_average.toFixed(1) == 0 ? "Coming Soon" : movie.vote_average.toFixed(1)}
                             </div>
                             <div className='h-[5vh] text-slate-50 font-bold w-full pl-2 pr-2 mt-2'>
                                 {movie.title.length > 50 ? movie.title.substring(0, 50) + "..." : movie.title}
@@ -64,13 +76,14 @@ function Movies() {
                             <div className='w-[170px] h-[5vh] text-sky-600 text-center p-1 bg-opacity-40 bg-black rounded-l m-2 mt-12'>
                                 
                                 {
-                                    watchlist.includes(movie.id) === false ? (
-                                        <button onClick={() => addToWatchlist(movie.id)} ><span className='pr-2'><FontAwesomeIcon icon={faPlus}/></span>Watchlist</button>
+                                    watchlist.some(item => {return JSON.stringify(item) === JSON.stringify(movie)})  ? (
+                                        <button onClick={() => removeFromWatchlist(movie)} ><span className='pr-2'><FontAwesomeIcon icon={faCheck} /></span>Watchlist</button>
+                                        
                                     ) : (
-                                        <button onClick={() => removeFromWatchlist(movie.id)} ><span className='pr-2'><FontAwesomeIcon icon={faCheck}/></span>Watchlist</button>
+                                        <button onClick={() => addToWatchlist(movie)} ><span className='pr-2'><FontAwesomeIcon icon={faPlus} /></span>Watchlist</button>
                                     )
                                 }
-                                
+
                             </div>
                         </div>
                     </div>
@@ -78,11 +91,11 @@ function Movies() {
 
 
             </div>
-            
-            
+
+
             <Pagination onPageChange={fetchMovies} />
-            
-            
+
+
         </div>
     )
 }
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-function Watchlist(props) {
+function Watchlist() {
 
-  //let { watchlist } = props
+  const [favorites, setFavorites] = useState([])
 
-  console.log("inside watchlist component ", props.watchlist);
+  useEffect(() => {
+    let moviesFromLocalstorage = localStorage.getItem('imdb')
+    moviesFromLocalstorage = JSON.parse(moviesFromLocalstorage)
+
+    setFavorites(moviesFromLocalstorage)
+  }, [])
 
   return (
     <div className='flex justify-center w-full bg-gray-300'>
@@ -15,34 +20,40 @@ function Watchlist(props) {
           Your Watchlist
         </div>
         <div>
+          {
+            favorites.map((favMovie) => {
+              return <div key={favMovie.id} className='flex h-[35vh] border-b-2 p-4 mb-3'>
+                <div className='rounded-xl w-[17vh] h-[25vh] bg-center bg-cover md:h[40vh] md:w[180px]'
+                  style={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${favMovie.poster_path})`
+                  }}>
 
-          <div className='flex h-[200px] border-b-2 p-4 mb-3'>
-            <div className='rounded-xl w-[17vh] h-[25vh] bg-center bg-cover md:h[40vh] md:w[180px]'
-              style={{
-                backgroundImage: `url(https://m.media-amazon.com/images/M/MV5BNjQwZDIyNjAtZGQxMC00OTUwLWFiOWYtNzg2NDc5Mjc1MDQ5XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_FMjpg_UX540_.jpg)`
-              }}>
+                </div>
+                <div className='ml-3 w-[100vh]'>
+                  <div className='text-xl text-sky-600 font-bold'>
+                    {favMovie.title.length > 50 ? favMovie.title.substring(0, 50) + "..." : favMovie.title}
+                  </div>
+                  <div className='text-zinc-400 text-xs'>
+                    {favMovie.release_date.substring(0,4)} 
+                    {/* | 1h 46m | G | Animation, Adventure, Comedy */}
+                  </div>
+                  <div className='pt-1'>
+                    <span className='pr-2'><FontAwesomeIcon icon={faStar} color='gold' /></span>
+                    {favMovie.vote_average.toFixed(1) == 0 ? "Coming Soon" : favMovie.vote_average.toFixed(1)}
+                  </div>
+                  {/* <div className='text-sky-600'>
+                    John Lasseter, Bradford Lewis | Owen Wilson, Larry the Cable Guy, Michael Caine
+                  </div> */}
+                  <div className='text-justify pt-2'>
+                    {favMovie.overview.length > 400 ? favMovie.overview.substring(0, 400) + "..." : favMovie.overview}
+                  </div>
+                </div>
+              </div>
+            })
+          }
 
-            </div>
-            <div className='ml-3 w-[100vh]'>
-              <div className='text-xl text-sky-600 font-bold'>Love Again</div>
-              <div className='text-zinc-400 text-xs'>
-                2023 | 1h 46m | G | Animation, Adventure, Comedy
-              </div>
-              <div>
-                <span className='pr-2'><FontAwesomeIcon icon={faStar} color='gold' /></span>
-                6.2
-              </div>
-              <div className='text-sky-600'>
-                John Lasseter, Bradford Lewis | Owen Wilson, Larry the Cable Guy, Michael Caine
-              </div>
-              <div>
-                Star race car Lightning McQueen and his pal Mater head overseas to compete in the World Grand Prix race. But the road to the championship becomes rocky as Mater gets caught up in an intriguing adventure of his own: international espionage.
 
-              </div>
-            </div>
-          </div>
 
-          
 
         </div>
       </div>
